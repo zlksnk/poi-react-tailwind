85fa810463e93ccf8f73d8abde4617039d6a4e69
Ajit Singh <ajit.singh2905@gmail.com>
2018-10-28 20:47:46 +0530

    init

---
+++ poi.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: "./src/static/index.html",
+  pages: {
+    index: {
+      entry: './src/main.js',
+    }
+  }
+}
---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "@/static/css/tailwind.css";
+
+const App = () => {
+  return (
+    <div className="font-sans">
+      <h1 className="text-blue-dark font-light text-center mt-5 text-4xl">
+        All set!
+        <span role="img" aria-label="party popper">
+          🎉
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default App;
